3f205945901600a7b22d9c4b4a315091e93aa4c4
CharlieSandewo <sandewocharlie@gmail.com>
2024-08-20 21:25:06 +0700

    ref: wallet amount to be auto updated depneding on transactions created

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,16 @@
 class Wallet < ApplicationRecord
   belongs_to :entity
+
+  after_save :update_amount
+
+  def update_amount
+    update(:amount, calculate_amount)
+  end
+
+  def calculate_amount
+    credits = Transactions::CreditTransaction.sum(:amount) || 0.0
+    debits = Transactions::DebitTransaction.sum(:amount) || 0.0
+
+    (credits - debits).to_f
+  end
 end
